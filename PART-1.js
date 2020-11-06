48ea5df45fb5bcc78058af58da0326dacc0d14f2
ANUNAY SINHA <55510402+anusu90@users.noreply.github.com>
2020-11-06 23:38:23 +0530

    Update PART-1.js

--- PART-1.js
+++ PART-1.js
@@ -1,35 +1,39 @@
 // ## HOW DO WE DO COPY BY VALUE
 
-//COPY BY REFERENCE
-let a = [1,2,3,4];
-let b = a;
-b[1] = 45;
-console.log(a,b);
+CopyByValueOrRef = function (){
+    //COPY BY REFERENCE
+    let a = [1,2,3,4];
+    let b = a;
+    b[1] = 45;
+    console.log(a,b);
+    
+    //COPY BY VALUE - METHOD #1 - Using Spread operator
+    let c = [...a];
+    c[1]=55;
+    console.log(a,c);
 
-//COPY BY VALUE - METHOD #1 - Using Spread operator
-let c = [...a];
-c[1]=55;
-console.log(a,c);
+    //COPY BY VALUE - METHOD #2 - Using Object.assign
+    let d =[]
+    Object.assign(d,a);
+    d[1] = 65;
+    console.log(a,d)
 
-//COPY BY VALUE - METHOD #2 - Using Object.assign
-let d =[]
-Object.assign(d,a);
-d[1] = 65;
-console.log(a,d)
-
-//COPY BY VALUE - METHOD #3 - Using map
-let e = a.map(v=>v)
-e[1] = 75;
-console.log(a,e);
+    //COPY BY VALUE - METHOD #3 - Using map
+    let e = a.map(v=>v)
+    e[1] = 75;
+    console.log(a,e);
 
 
-//COPY BY VALUE - METHOD #4 - Using for loop
-let f = [];
-for (var [index,value] of a.entries()){
-    f[index] = value;
+    //COPY BY VALUE - METHOD #4 - Using for loop
+    let f = [];
+    for (var [index,value] of a.entries()){
+        f[index] = value;
+    }
+    f[1] = 85
+    console.log(a,f);
 }
-f[1] = 85
-console.log(a,f);
+
+CopyByValueOrRef()
 
 /* THE OUTPUT IS AS FOLLOWING
 
@@ -43,16 +47,21 @@ console.log(a,f);
 
 // NEXT PART OF QUESTION
 
-console.log(typeof(1))
-console.log(typeof(1.1))
-console.log(typeof("1.1"))
-console.log(typeof(true))
-console.log(typeof(null))
-console.log(typeof(undefined))
-console.log(typeof([]))
-console.log(typeof({}))
+CheckTypeOf = function () {
+    console.log(typeof(1))
+    console.log(typeof(1.1))
+    console.log(typeof("1.1"))
+    console.log(typeof(true))
+    console.log(typeof(null))
+    console.log(typeof(undefined))
+    console.log(typeof([]))
+    console.log(typeof({}))
 
-/* THE OUT PUT WAS
+}
+
+CheckTypeOf()
+
+/* THE OUTPUT WAS
 
 number
 number
